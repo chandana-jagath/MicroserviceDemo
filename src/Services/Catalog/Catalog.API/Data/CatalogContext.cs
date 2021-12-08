ed714fa517877c7ddb934295b244bb708a611020
Chandana Jagath <pushpakumara.chandana@gmail.com>
2021-12-08 17:02:07 +0530

    Catalog micro service db

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSetteings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSetteings:DatabaseName"));
+
+            Products=database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSetteings:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
