0bb1fbbde8c7bc9ab92158d8a4b54741dcbe7f4a
Chandana Jagath <pushpakumara.chandana@gmail.com>
2021-12-24 15:37:22 +0530

    Web appication

--- src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -27,11 +27,11 @@ namespace Ordering.Infrastructure.Persistence
                 {
                     case EntityState.Added:
                         entry.Entity.CreatedDate = DateTime.Now;
-                        entry.Entity.CreatedBy = "swn";
+                        entry.Entity.CreatedBy = "Admin";
                         break;
                     case EntityState.Modified:
                         entry.Entity.LastModifiedDate = DateTime.Now;
-                        entry.Entity.LastModifiedBy = "swn";
+                        entry.Entity.LastModifiedBy = "Admin";
                         break;
                 }
             }
